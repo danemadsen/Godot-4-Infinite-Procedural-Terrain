b88cb502435f1b029926dfaa1613b204e4166a85
Dane Madsen <dane_madsen@hotmail.com>
2022-07-11 17:36:22 +1000

    Fixed weird gaps between chunks

---
+++ Scripts/Main.gd
@@ -0,0 +1,99 @@
+extends Node3D
+
+var chunks = {}
+var unready_chunks = {}
+var thread
+
+var base_noise
+@export var chunk_size = 128
+@export var chunk_height = 32
+@export var chunk_amount = 16
+@export var base_noise_seed = 88
+@export var base_noise_frequency = 0.005
+@export var base_noise_fractal_octaves = 5
+@export var base_noise_fractal_lacunarity = 2.5
+@export var base_noise_fractal_gain = 0.6
+@export var base_noise_fractal_weighted_strength = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+	base_noise = FastNoiseLite.new()
+	base_noise.noise_type = FastNoiseLite.TYPE_SIMPLEX
+	base_noise.seed = base_noise_seed
+	base_noise.frequency = base_noise_frequency
+	base_noise.fractal_type = FastNoiseLite.FRACTAL_FBM
+	base_noise.fractal_octaves = base_noise_fractal_octaves
+	base_noise.fractal_lacunarity = base_noise_fractal_lacunarity
+	base_noise.fractal_gain = base_noise_fractal_gain
+	base_noise.fractal_weighted_strength = base_noise_fractal_weighted_strength
+	
+	thread = Thread.new()
+
+func add_chunk(x, z):
+	var key = str(x) + "," + str(z)
+	if chunks.has(key) or unready_chunks.has(key):
+		return
+	
+	if not thread.is_started():
+		var callable = Callable(self, "load_chunk")
+		thread.start(callable)
+		callable.call([thread, x, z])
+		unready_chunks[key] = 1
+
+func load_chunk(arr):
+	var thread = arr[0]
+	var x = arr[1]
+	var z = arr[2]
+	
+	var chunk = Chunk.new(base_noise, x * chunk_size, z * chunk_size, chunk_size, chunk_height)
+	chunk.position = Vector3(x * chunk_size, 0, z * chunk_size)
+	
+	call_deferred("load_done", chunk, thread)
+
+func load_done(chunk, thread):
+	add_child(chunk)
+	var key = str(chunk.x / chunk_size) + "," + str(chunk.z / chunk_size)
+	chunks[key] = chunk
+	unready_chunks.erase(key)
+	thread.wait_to_finish()
+
+func get_chunk(x, z):
+	var key = str(x) + "," + str(z)
+	
+	if chunks.has(key):
+		return chunks.get(key)
+		
+	return null
+
+func _process(delta):
+	update_chunks()
+	clean_up_chunks()
+	reset_chunks()
+
+func _input(event):
+	if Input.is_key_pressed(KEY_ESCAPE):
+		get_tree().quit()
+
+func update_chunks():
+	var player_translation = $Player.position
+	var p_x = int(player_translation.x) / chunk_size
+	var p_z = int(player_translation.z) / chunk_size
+	
+	for x in range(p_x - chunk_amount * 0.5, p_x + chunk_amount * 0.5):
+		for z in range(p_z - chunk_amount * 0.5, p_z + chunk_amount * 0.5):
+			add_chunk(x, z)
+			var chunk = get_chunk(x, z)
+			if chunk != null:
+				chunk.should_remove = false
+
+func clean_up_chunks():
+	for key in chunks:
+		var chunk = chunks[key]
+		if chunk.should_remove:
+			chunk.queue_free()
+			chunks.erase(key)
+
+func reset_chunks():
+	for key in chunks:
+		chunks[key].should_remove = true
---
+++ Scripts/NoClipPlayer.gd
@@ -0,0 +1,40 @@
+extends Camera3D
+# Simple script for a flying first-person player
+
+
+@export var look_sens : float = 0.1
+@export var speed : float = 60.0
+var _look_buffer = Vector2()
+
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func _physics_process(delta):
+	var direction = (
+			Input.get_axis("down", "up") * Vector3.FORWARD + 
+			Input.get_axis("left", "right") * Vector3.RIGHT +
+			Input.get_axis("sprint", "jump") * Vector3.UP
+	)
+	
+	direction = direction.normalized().rotated(Vector3.UP, rotation.y)
+	position += direction * delta * speed
+	
+	if not _look_buffer == Vector2.ZERO:
+		rotation.y -= _look_buffer.x * delta * look_sens
+		rotation.x -= _look_buffer.y * delta * look_sens
+		rotation.x = clamp(rotation.x, - PI/2, PI/2)
+		_look_buffer = Vector2.ZERO
+
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		_look_buffer += event.relative
+
+
+func _process(_delta):
+	if Input.is_action_just_pressed("toggle_fullscreen"):
+		OS.window_fullscreen = not OS.window_fullscreen
+	if Input.is_action_just_pressed("pause"): 
+		get_tree().notification(MainLoop.NOTIFICATION_WM_QUIT_REQUEST)
