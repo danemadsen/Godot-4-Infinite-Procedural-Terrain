b0ac6d5a3cc34d121f573790581044715b0bf9b5
Dane Madsen <dane_madsen@hotmail.com>
2022-07-14 22:24:38 +1000

    Worked on adding biomes

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -4,30 +4,16 @@ var chunks = {}
 var unready_chunks = {}
 var thread
 
-var base_noise
+var noise
 @export var chunk_size = 128
 @export var chunk_height = 32
 @export var chunk_amount = 16
-@export var base_noise_seed = 88
-@export var base_noise_frequency = 0.005
-@export var base_noise_fractal_octaves = 5
-@export var base_noise_fractal_lacunarity = 2.5
-@export var base_noise_fractal_gain = 0.6
-@export var base_noise_fractal_weighted_strength = 1
+@export var seed = 148
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	base_noise = FastNoiseLite.new()
-	base_noise.noise_type = FastNoiseLite.TYPE_SIMPLEX
-	base_noise.seed = base_noise_seed
-	base_noise.frequency = base_noise_frequency
-	base_noise.fractal_type = FastNoiseLite.FRACTAL_FBM
-	base_noise.fractal_octaves = base_noise_fractal_octaves
-	base_noise.fractal_lacunarity = base_noise_fractal_lacunarity
-	base_noise.fractal_gain = base_noise_fractal_gain
-	base_noise.fractal_weighted_strength = base_noise_fractal_weighted_strength
-	
+	noise = NoiseMaps.new(seed)
 	thread = Thread.new()
 
 func add_chunk(x, z):
@@ -46,7 +32,7 @@ func load_chunk(arr):
 	var x = arr[1]
 	var z = arr[2]
 	
-	var chunk = Chunk.new(base_noise, x * chunk_size, z * chunk_size, chunk_size, chunk_height)
+	var chunk = Chunk.new(noise.base, x * chunk_size, z * chunk_size, chunk_size, chunk_height)
 	chunk.position = Vector3(x * chunk_size, 0, z * chunk_size)
 	
 	call_deferred("load_done", chunk, thread)
